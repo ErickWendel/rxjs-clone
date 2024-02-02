4cc90fda1cfead5b60eb83736993b249cbd7cb29
Erick Wendel <erick.workspace@gmail.com>
2024-02-02 20:30:44 -0300

    add working class
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- index.js
+++ index.js
@@ -1,40 +1,57 @@
-import { race, fromEvent, interval, map, switchMap, takeUntil } from "./operators.js";
+import { fromEvent, interval, map, merge, switchMap, takeUntil } from "./operators.js"
 
-const canvas = document.getElementById("canvas");
-const clearBtn = document.getElementById("clearBtn");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('canvas')
+const clearBtn = document.getElementById('clearBtn')
+const ctx = canvas.getContext('2d')
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-const resetCanvas = (width, height) => {
-    const parent = canvas.parentElement
-
-    canvas.width = width || parent.clientWidth * 0.9
-    canvas.height = height || parent.clientHeight * 1.5
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.strokeStyle = "green";
-    ctx.lineWidth = 4;
-}
-resetCanvas()
-
-const getMousePos = (canvasDom, mouseEvent) => {
-    const rect = canvasDom.getBoundingClientRect();
-    return {
-        x: mouseEvent.clientX - rect.left,
-        y: mouseEvent.clientY - rect.top
-    }
-}
 const mouseEvents = {
     down: 'mousedown',
     move: 'mousemove',
     up: 'mouseup',
     leave: 'mouseleave',
+
     touchstart: 'touchstart',
     touchmove: 'touchmove',
     touchend: 'touchend',
-    click: 'click'
+
+    click: 'click',
 }
+
+const getMousePosition = (canvasDom, eventValue) => {
+    const rect = canvasDom.getBoundingClientRect()
+    return {
+        x: eventValue.clientX - rect.left,
+        y: eventValue.clientY - rect.top,
+    }
+
+}
+const resetCanvas = (width, height) => {
+    const parent = canvas.parentElement
+    canvas.width = width || parent.clientWidth * 0.9
+    canvas.height = height || parent.clientHeight * 1.5
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.strokeStyle = 'green'
+    ctx.lineWidth = 4
+}
+
+resetCanvas()
+
+const sleep = (ms) => new Promise(r => setTimeout(r, ms))
+
+const store = {
+    db: [],
+    get() {
+        return this.db
+    },
+    set(item) {
+        this.db.unshift(item)
+    },
+    clear() {
+        this.db.length = 0
+    }
+}
+
 const touchToMouse = (touchEvent, mouseEvent) => {
     const [touch] = touchEvent.touches.length ?
         touchEvent.touches :
@@ -42,54 +59,48 @@ const touchToMouse = (touchEvent, mouseEvent) => {
 
     return new MouseEvent(mouseEvent, {
         clientX: touch.clientX,
-        clientY: touch.clientY
+        clientY: touch.clientY,
     })
+
 }
 
-const store = {
-    db: [],
-    set(position) {
-        this.db.unshift(position)
-    },
-    get() {
-        return this.db
-    },
-    clean() {
-        this.db.length = 0
-    }
-}
-
-
-race([
+merge([
     fromEvent(canvas, mouseEvents.down),
     fromEvent(canvas, mouseEvents.touchstart)
-        .pipeThrough(map(e => touchToMouse(e, mouseEvents.down))),
+        .pipeThrough(map(e => touchToMouse(e, mouseEvents.touchstart)))
 ])
     .pipeThrough(
-        switchMap((e) => {
-            return race([
+        switchMap(e => {
+            return merge([
                 fromEvent(canvas, mouseEvents.move),
                 fromEvent(canvas, mouseEvents.touchmove)
-                    .pipeThrough(map(e => touchToMouse(e, mouseEvents.move))),
+                    .pipeThrough(map(e => touchToMouse(e, mouseEvents.move)))
             ])
                 .pipeThrough(
                     takeUntil(
-                        race([
+                        merge([
                             fromEvent(canvas, mouseEvents.up),
                             fromEvent(canvas, mouseEvents.leave),
                             fromEvent(canvas, mouseEvents.touchend)
-                                .pipeThrough(map(e => touchToMouse(e, mouseEvents.up))),
+                                .pipeThrough(map(e => touchToMouse(e, mouseEvents.up)))
                         ])
                     )
                 )
+            // / }, { pairwise: false })
         })
+
     )
+
     .pipeThrough(
         map(function ([mouseDown, mouseMove]) {
-            this._lastPosition = this._lastPosition ?? mouseDown;
+            this._lastPosition = this._lastPosition ?? mouseDown
 
-            const [from, to] = [this._lastPosition, mouseMove].map(item => getMousePos(canvas, item))
-            this._lastPosition = mouseMove.type === mouseEvents.up ? null : mouseMove
+            const [from, to] = [this._lastPosition, mouseMove]
+                .map(item => getMousePosition(canvas, item))
+
+            this._lastPosition = mouseMove.type === mouseEvents.up ?
+                null :
+                mouseMove
 
             return { from, to }
         })
@@ -97,27 +108,28 @@ race([
     .pipeTo(new WritableStream({
         write({ from, to }) {
             store.set({ from, to })
-            ctx.moveTo(from.x, from.y);
-            ctx.lineTo(to.x, to.y);
-            ctx.stroke();
+            ctx.moveTo(from.x, from.y)
+            ctx.lineTo(to.x, to.y)
+            ctx.stroke()
         }
     }))
 
+// clear
 
 fromEvent(clearBtn, mouseEvents.click)
     .pipeTo(new WritableStream({
-        async write() {
+        async write(chunk) {
             ctx.beginPath()
-            ctx.strokeStyle = 'white';
+            ctx.strokeStyle = 'white'
 
             for (const { from, to } of store.get()) {
-                ctx.moveTo(from.x, from.y);
-                ctx.lineTo(to.x, to.y);
-                ctx.stroke();
+                ctx.moveTo(from.x, from.y)
+                ctx.lineTo(to.x, to.y)
+                ctx.stroke()
+
                 await sleep(5)
             }
-
             resetCanvas(canvas.width, canvas.height)
-            store.clean()
+            store.clear()
         }
     }))
--- operators.js
+++ operators.js
@@ -1,11 +1,13 @@
+
 /**
+ *
  * @param {EventTarget} target
  * @param {string} eventName
  * @returns {ReadableStream}
  */
 const fromEvent = (target, eventName) => {
-    let _listener;
-    const readable = new ReadableStream({
+    let _listener
+    return new ReadableStream({
         start(controller) {
             _listener = (e) => controller.enqueue(e)
             target.addEventListener(eventName, _listener)
@@ -14,85 +16,31 @@ const fromEvent = (target, eventName) => {
             target.removeEventListener(eventName, _listener)
         }
     })
-
-    return readable
-}
-
-/**
- * @typedef {function(): ReadableStream | TransformStream} StreamFunction
- *
- * @param {StreamFunction} fn
- * @param {object} options
- * @param {boolean} options.pairwise
- * set default pairwise to true
- * @returns {TransformStream}
- */
-const switchMap = (fn, options = { pairwise: true }) => {
-    return new TransformStream({
-        transform(chunk, controller) {
-            const stream = fn(chunk)
-            // can be a readable or a TransformStream.readable
-            const reader = (stream.readable || stream).getReader()
-
-            return (async function read() {
-                const { value, done } = await reader.read()
-                if (done) return
-
-                const result = options.pairwise ? [chunk, value] : value
-                controller.enqueue(result)
-
-                return read()
-            })()
-        },
-    })
-}
-
-/**
- * @param {ReadableStream | TransformStream} stream
- * @returns {TransformStream}
- */
-const takeUntil = (stream) => {
-    const readAndTerminate = async (stream, controller) => {
-        const reader = (stream.readable || stream).getReader()
-        const { value } = await reader.read()
-
-        controller.enqueue(value)
-        controller.terminate()
-    }
-
-    return new TransformStream({
-        start(controller) {
-            readAndTerminate(stream, controller)
-        },
-        transform(chunk, controller) {
-            controller.enqueue(chunk)
-        },
-    })
 }
 
 /**
  * @param {Number} ms
- * @returns {TransformStream}
+ * @returns {ReadableStream}
  */
 const interval = (ms) => {
-    let intervalId
-    return new TransformStream({
+    let _intervalId
+    return new ReadableStream({
         start(controller) {
-            intervalId = setInterval(
-                () =>
-                    controller.enqueue(Date.now()),
-                ms
-            )
+            _intervalId = setInterval(() => {
+                controller.enqueue(Date.now())
+            }, ms)
         },
         cancel() {
-            clearInterval(intervalId)
+            clearInterval(_intervalId)
         }
     })
 }
 
+
 /**
+ *
  * @param {Function} fn
- * @returns {TransformStream}
+ * @return {TransformStream}
  */
 const map = (fn) => {
     return new TransformStream({
@@ -103,26 +51,84 @@ const map = (fn) => {
 }
 
 /**
+ *
  * @typedef {ReadableStream | TransformStream} Stream
  * @param {Stream[]} streams
  * @returns {ReadableStream}
  */
-const race = (streams) => {
+const merge = (streams) => {
     return new ReadableStream({
-        start(controller) {
+        async start(controller) {
             for (const stream of streams) {
                 const reader = (stream.readable || stream).getReader()
+                async function read() {
+                    const { value, done } = await reader.read()
+                    if (done) return
+                    // verifica se a stream ja encerrou
+                    if(!controller.desiredSize) return
 
-                    ; (async function read() {
-                        const { value, done } = await reader.read()
-                        if (done) return
-                        // stream is done
-                        if (!controller.desiredSize) return
-                        controller.enqueue(value)
+                    controller.enqueue(value)
 
-                        return read()
-                    })()
+                    return read()
+                }
+
+                read()
             }
+        }
+    })
+}
+
+/**
+ * @typedef {function(): ReadableStream | TransformStream} StreamFunction
+ *
+ * @param {StreamFunction} fn
+ * @param {object} options
+ * @param {boolean} options.pairwise
+ *
+ * @return {TransformStream}
+ */
+const switchMap = (fn, options = { pairwise: true }) => {
+    return new TransformStream({
+        // mousedown
+        transform(chunk, controller) {
+            const stream = fn.bind(fn)(chunk)
+
+            const reader = (stream.readable || stream).getReader()
+            async function read() {
+                // mousemove
+                const { value, done } = await reader.read()
+                if (done) return
+                const result = options.pairwise ? [chunk, value] : value
+                controller.enqueue(result)
+
+                return read()
+            }
+
+            return read()
+
+        }
+    })
+}
+
+/**
+ *
+ * @param {ReadableStream | TransformStream} stream
+ * @returns {TransformStream}
+ */
+const takeUntil = (stream) => {
+    const readAndTerminate = async (stream, controller) => {
+        const reader = (stream.readable || stream).getReader()
+        const { value } = await reader.read()
+        controller.enqueue(value)
+        controller.terminate()
+    }
+
+    return new TransformStream({
+        start(controller) {
+            readAndTerminate(stream, controller)
+        },
+        transform(chunk, controller) {
+            controller.enqueue(chunk)
         }
     })
 }
@@ -131,7 +137,7 @@ export {
     fromEvent,
     interval,
     map,
-    race,
+    merge,
     switchMap,
     takeUntil,
 }
